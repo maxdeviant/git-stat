5e2cf20014ce0926329895257d7f7338942319b5
Marshall Bowers <elliott.codes@gmail.com>
2016-03-23 21:14:39 -0400

    Begin adding test harness

---
+++ test/spec/api.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const should = require('should');
+const api = require('../../lib/api');
+
+describe('api', () => {
+    describe('getCommitDates', () => {
+        xit('should be gather commits for the specified year', () => {
+            return api.getCommitDates({
+
+            });
+        });
+    });
+});
