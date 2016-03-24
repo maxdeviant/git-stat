3696ae65525bca458e404fad0693ea66c58fc04d
Marshall Bowers <elliott.codes@gmail.com>
2016-03-23 21:43:07 -0400

    Add ability to get commits at month and day levels
    Closes #1, closes #2.

--- test/spec/api.js
+++ test/spec/api.js
@@ -4,11 +4,35 @@ const should = require('should');
 const api = require('../../lib/api');
 
 describe('api', () => {
-    describe('getCommitDates', () => {
-        xit('should be gather commits for the specified year', () => {
-            return api.getCommitDates({
+    describe('buildDateRange', () => {
+        it('should build a date range for the specified day', () => {
+            let afterDate = new Date(2016, 4, 16);
+            let beforeDate = new Date(2016, 4, 17);
 
-            });
+            let dateRange = api.buildDateRange(2016, 5, 16);
+
+            (dateRange.after).should.be.exactly(afterDate.toISOString());
+            (dateRange.before).should.be.exactly(beforeDate.toISOString());
+        });
+
+        it('should build a date range for the specified month', () => {
+            let afterDate = new Date(2016, 4, 1);
+            let beforeDate = new Date(2016, 4, 31);
+
+            let dateRange = api.buildDateRange(2016, 5, null);
+
+            (dateRange.after).should.be.exactly(afterDate.toISOString());
+            (dateRange.before).should.be.exactly(beforeDate.toISOString());
+        });
+
+        it('should build a date range for the specified year', () => {
+            let afterDate = new Date(2016, 0, 1);
+            let beforeDate = new Date(2017, 0, 1);
+
+            let dateRange = api.buildDateRange(2016, null, null);
+
+            (dateRange.after).should.be.exactly(afterDate.toISOString());
+            (dateRange.before).should.be.exactly(beforeDate.toISOString());
         });
     });
 });
